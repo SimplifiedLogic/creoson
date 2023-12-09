c85135c4f63295e2262659da55710b1f6b8673b0
Administrator <adama@simplifiedlogic.com>
2023-12-09 17:20:36 -0500

    fix classpath in jni script

--- jshell_native/jni_creoson.bat
+++ jshell_native/jni_creoson.bat
@@ -1,7 +1,12 @@
 @echo off
 
-set cp=..\creoson-core\src
-set cp=%cp%;..\creoson-intf\src
+set cp=.
+setlocal enabledelayedexpansion
+for %%x in (..\out\*.jar) DO (
+  set cp=!cp!;%%x
+)
+rem set cp=..\out\creoson-core*.jar
+rem set cp=%cp%;..\out\creoson-intf*.jar
 set cp=%cp%;d:\Program Files\ptc\Creo 9.0.0.0\Common Files\text\java\pfcasync.jar
 
 set outdir=jshell_native
