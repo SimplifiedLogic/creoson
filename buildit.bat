1fc35e6540492dedaa5ea27f462ba78154feff41
Administrator <adama@simplifiedlogic.com>
2025-11-23 00:32:58 -0500

    release notes and version bumps

--- buildit.bat
+++ buildit.bat
@@ -1,8 +1,9 @@
-rem MUST COMPILE WITH JAVA 1.7 OR LOWER BECAUSE THAT'S WHAT THE CUSTOMER HAS INSTALLED
+rem COMPILING WITH JAVA 1.7 FOR BACKWARD COMPATIBILITY, BUT EMBEDDED JRE WILL BE 21
 
 @echo off
-set path=d:\apache-ant-1.7.1\bin;d:\java64\jdk1.7.0_79\bin;%path%
-
 set JAVA_HOME=d:\java64\jdk1.7.0_79
+rem set JAVA_HOME=d:\java64\jdk-21.0.8
+
+set path=d:\apache-ant-1.7.1\bin;%JAVA_HOME%\bin;%path%
 
 ant -f build-all.xml
